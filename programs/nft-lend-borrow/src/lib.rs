76834c22fa6d821e4bbd3c5fa26de59b1d2c46be
thestriver <wannabeamumin@gmail.com>
2023-11-24 17:42:51 -0700

    NFT Lending and Borrowing

---
+++ programs/nft-lend-borrow/src/lib.rs
@@ -0,0 +1,47 @@
+pub mod errors;
+pub mod instructions;
+pub mod states;
+
+use anchor_lang::prelude::*;
+
+pub use errors::ErrorCodes;
+pub use instructions::*;
+pub use states::*;
+
+declare_id!("6EYYcsG5A1GpABbuaLJ1VWxhWavNfK1CVhWi1hyTPoJ3");
+
+#[program]
+pub mod nft_lend_borrow {
+    use super::*;
+
+    pub fn create_pool(
+        ctx: Context<CreatePool>,
+        collection_id: Pubkey,
+        duration: i64,
+    ) -> Result<()> {
+        instructions::create_pool::handler(ctx, collection_id, duration)
+    }
+
+    pub fn offer_loan(ctx: Context<OfferLoan>, offer_amount: u64) -> Result<()> {
+        instructions::offer_loan::handler(ctx, offer_amount)
+    }
+
+    pub fn withdraw_offer(
+        ctx: Context<WithdrawOffer>,
+        minimum_balance_for_rent_exemption: u64,
+    ) -> Result<()> {
+        instructions::withdraw_offer::handler(ctx, minimum_balance_for_rent_exemption)
+    }
+
+    pub fn borrow(ctx: Context<Borrow>, minimum_balance_for_rent_exemption: u64) -> Result<()> {
+        instructions::borrow::handler(ctx, minimum_balance_for_rent_exemption)
+    }
+
+    pub fn repay(ctx: Context<Repay>) -> Result<()> {
+        instructions::repay::handler(ctx)
+    }
+
+    pub fn liquidate(ctx: Context<Liquidate>) -> Result<()> {
+        instructions::liquidate::handler(ctx)
+    }
+}
